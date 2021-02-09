6904765635156c9ef683f54f1ee0c2a659f050d0
anqixiang <anqixiang@163.com>
2021-02-09 23:36:20 +0800

    sync

---
+++ k8sPlatform/k8sPlatform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for k8sPlatform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'k8sPlatform.settings')
+
+application = get_asgi_application()
