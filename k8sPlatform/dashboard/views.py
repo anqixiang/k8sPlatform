3c498d5ea322f79465b626f784489d19bdbf130b
anqixiang <anqixiang@163.com>
2021-02-12 12:04:21 +0800

    20210202/12/211204

--- k8sPlatform/dashboard/views.py
+++ k8sPlatform/dashboard/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from kubernetes import client,config
 import os,hashlib,random
+from k8sPlatform import k8s
 
 # Create your views here.
+@k8s.self_login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -15,39 +17,41 @@ def login(request):
         # 验证token
         token = request.POST.get("token", None)   # 获取前端的token
         if token:
-            conf = client.Configuration()
-            conf.host = "https://192.168.137.2:6443"    # apiserver地址
-            conf.ssl_ca_cert = r"E:\学习\python\django\project\k8sPlatform\k8sPlatform\ca.crt"    # CA证书
-            conf.verify_ssl = True      # 启用证书验证，必须指定CA证书
-            conf.api_key = {"authorization": "Bearer " + token}     # 指定token
-            client.Configuration.set_default(conf)
-
-            try:
-                core_api = client.CoreApi()
-                core_api.get_api_versions()     # 查询api版本
+            if k8s.auth_check("token", token):
+                # 定义sesseion信息
+                request.session['is_login'] = True
+                request.session['auth_type'] = 'token'
+                request.session['token'] = token
+                # 返回前端消息
                 code = 0
                 msg = "认证成功"
-            except Exception as e:
+            else:
                 code = 1
                 msg = "token无效！"
         else:
             file_obj = request.FILES.get("file")    # 获取前端上传的文件
-            random_str = hashlib.md5(str(random.random()).encode()).hexdigest()     # 生成经过MD5加密的随机数
-            file_path = os.path.join('kubeconfig', random_str)  # 拼接路径
+            random_str = hashlib.md5(str(random.random()).encode()).hexdigest()     # 生成经过MD5加密的随机数,用作kubeconfig的名字
+            file_path = os.path.join("kubeconfig", random_str)
             try:
                 with open(file_path, 'w', encoding="utf8") as f:
                     data = file_obj.read().decode()     # bytes转str
                     f.write(data)
-                # 连接K8S
-                config.load_kube_config(r"%s" %file_path)
-                core_api = client.CoreApi()
-                core_api.get_api_versions()
-                code = 0
-                msg = "认证成功"
+
             except Exception:
                 code = 1
-                msg = "认证文件无效"
-                os.remove(file_path)    # 删除无效文件
-
+                msg = "文件类型错误！"
+                res = {"code": code, "msg": msg}
+                return JsonResponse(res)
+            if k8s.auth_check("kubeconfig", random_str):
+                # 定义sesseion信息
+                request.session['is_login'] = True
+                request.session['auth_type'] = 'kubeconfig'
+                request.session['token'] = random_str
+                # 返回前端消息
+                code = 0
+                msg = "认证成功"
+            else:
+                code = 1
+                msg = "认证文件无效！"
         res = {"code": code, "msg": msg}
         return JsonResponse(res)
